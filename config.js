b3b56517f016dcc9507db81b7e7cdf40663ad883
NP123123 <np288888@gmail.com>
2024-05-03 14:30:44 +0700

    123123
    Signed-off-by: NP123123 <np288888@gmail.com>

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Đỗ Tuyết Nhi';
+const giftUrl = 'https://350new.com';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'My Frield';
+const contentCard = 'Chúc em sinh nhật tràn ngập niềm vui và những nụ cười. Những điều tuyệt vời nhất sẽ dành cho em không chỉ trong ngày hôm nay mà còn trong những ngày tiếp theo.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Wecomel https://www.facebook.com/vn.NguyenAnhPhuong
+// Các bạn muốn học lập trình thì tham gia nhóm tự học lập trình nhé: https://www.facebook.com/vn.NguyenAnhPhuong
+const giftImage = '12345678-.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
